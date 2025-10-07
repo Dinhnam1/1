dd4628aa522005e17ff9becfb675e10d71100f9e
DESKTOP-N7PKHP4\HP <nguyen081104@gmail.com>
2025-10-07 16:11:30 +0700

    animation product-img

--- assets/script.js
+++ assets/script.js
@@ -20,8 +20,7 @@ storageButtons.forEach(btn => {
   });
 });
 
-
-
+// Highlight heart-icon
 document.querySelectorAll('.wishlist i').forEach(el => {
   el.addEventListener('click', () => {
     el.classList.toggle('fa-regular'); // trái tim rỗng
@@ -29,3 +28,29 @@ document.querySelectorAll('.wishlist i').forEach(el => {
     el.parentElement.classList.toggle('active'); // đổi màu hồng
   });
 });
+
+
+function changeImage(element) {
+  // Lấy thẻ ảnh chính
+  const mainImg = document.querySelector('.main-images img');
+  
+  // Lấy đường dẫn ảnh của thumbnail vừa click
+  const newSrc = element.getAttribute('src');
+  
+  // Đổi ảnh chính thành ảnh đó
+  mainImg.setAttribute('src', newSrc);
+  
+  // Hiệu ứng khi đổi ảnh
+  mainImg.style.opacity = 0;
+  setTimeout(() => {
+    mainImg.setAttribute('src', newSrc);
+    mainImg.style.opacity = 1;
+  }, 150);
+
+  // Xóa viền các ảnh khác
+  const thumbs = document.querySelectorAll('.thumbs img');
+  thumbs.forEach(img => img.classList.remove('active-thumb'));
+
+  // Thêm viền cho ảnh được chọn
+  element.classList.add('active-thumb');
+}

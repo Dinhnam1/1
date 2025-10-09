ae6e2c6f571bb52335b581079d18702d086d8c38
DESKTOP-N7PKHP4\HP <nguyen081104@gmail.com>
2025-10-10 01:10:26 +0700

    Design

--- assets/script.js
+++ assets/script.js
@@ -1,56 +1,51 @@
 const buttons = document.querySelectorAll(".colors .color");
 
-buttons.forEach(btn => {
+buttons.forEach((btn) => {
   btn.addEventListener("click", () => {
-
-    buttons.forEach(b => b.classList.remove("active"));
+    buttons.forEach((b) => b.classList.remove("active"));
     btn.classList.add("active");
-
   });
 });
 
 const storageButtons = document.querySelectorAll(".storage button");
 
-storageButtons.forEach(btn => {
+storageButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
-
-    storageButtons.forEach(b => b.classList.remove("active"));
+    storageButtons.forEach((b) => b.classList.remove("active"));
     btn.classList.add("active");
-
   });
 });
 
 // Highlight heart-icon
-document.querySelectorAll('.wishlist i').forEach(el => {
-  el.addEventListener('click', () => {
-    el.classList.toggle('fa-regular'); // trái tim rỗng
-    el.classList.toggle('fa-solid');   // trái tim đặc
-    el.parentElement.classList.toggle('active'); // đổi màu hồng
+document.querySelectorAll(".wishlist i").forEach((el) => {
+  el.addEventListener("click", () => {
+    el.classList.toggle("fa-regular"); // trái tim rỗng
+    el.classList.toggle("fa-solid"); // trái tim đặc
+    el.parentElement.classList.toggle("active"); // đổi màu hồng
   });
 });
 
-
 function changeImage(element) {
   // Lấy thẻ ảnh chính
-  const mainImg = document.querySelector('.main-images img');
-  
+  const mainImg = document.querySelector(".main-images img");
+
   // Lấy đường dẫn ảnh của thumbnail vừa click
-  const newSrc = element.getAttribute('src');
-  
+  const newSrc = element.getAttribute("src");
+
   // Đổi ảnh chính thành ảnh đó
-  mainImg.setAttribute('src', newSrc);
-  
+  mainImg.setAttribute("src", newSrc);
+
   // Hiệu ứng khi đổi ảnh
   mainImg.style.opacity = 0;
   setTimeout(() => {
-    mainImg.setAttribute('src', newSrc);
+    mainImg.setAttribute("src", newSrc);
     mainImg.style.opacity = 1;
   }, 150);
 
   // Xóa viền các ảnh khác
-  const thumbs = document.querySelectorAll('.thumbs img');
-  thumbs.forEach(img => img.classList.remove('active-thumb'));
+  const thumbs = document.querySelectorAll(".thumbs img");
+  thumbs.forEach((img) => img.classList.remove("active-thumb"));
 
   // Thêm viền cho ảnh được chọn
-  element.classList.add('active-thumb');
+  element.classList.add("active-thumb");
 }
